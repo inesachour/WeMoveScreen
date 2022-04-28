638436038feeb430101936991d4e33bec181ec29
unknown <inesachour@insat.u-carthage.tn>
2022-04-28 08:58:29 +0200

    text styling

--- lib/reservation_card.dart
+++ lib/reservation_card.dart
@@ -6,41 +6,71 @@ class ReservationCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10)
+      ),
       child: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                    decoration: BoxDecoration(
-                        color: Colors.blueAccent,
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                    child: Column(
-                      children: [
-                        Text("Date1"),
-                        Text("Date2")
-                      ],
-                    )
-                ),
-                IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: (){},
-                ),
-              ],
-            ),
-            Text("test test"),
-            Text("test test"),
-            Text("test test"),
-            Row(
-              children: [
-                Text("price"),
-                Text("buttons + -")
-              ],
-            ),
-          ],
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+            image: AssetImage("assets/image.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                      height: 50,
+                      width: 75,
+                      decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Jeu 10", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 18),),
+                          Text("Fév", style: TextStyle(color: Colors.white, fontSize: 16))
+                        ],
+                      )
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: (){},
+                  ),
+                ],
+              ),
+              Text("Day Mega Passage", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 20),),
+              Text("Lyon Sport", style: TextStyle(color: Colors.white, fontSize: 15)),
+              Text("de 1h à 7h", style: TextStyle(color: Colors.white, fontSize: 16,fontWeight: FontWeight.w700)),
+              Text("Places réservés", style: TextStyle(color: Colors.white, fontSize: 15)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("price", style: TextStyle(color: Colors.white)),
+                  Row(
+                    children: [
+                      TextButton(
+                        child: Text("-"),
+                        onPressed: (){},
+                      ),
+                      Text("0", style: TextStyle(color: Colors.white)),
+                      TextButton(
+                        child: Text("+"),
+                        onPressed: (){},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
