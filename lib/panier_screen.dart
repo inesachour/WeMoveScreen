0df31c68b779f578df7046b1db0ced05354a5f69
unknown <inesachour@insat.u-carthage.tn>
2022-04-29 11:48:59 +0200

    navbar

--- lib/panier_screen.dart
+++ lib/panier_screen.dart
@@ -34,73 +34,115 @@ class _PanierScreenState extends State<PanierScreen> {
         elevation: 0,
 
       ),
-      body: Container(
-        color: primaryBackgroundColor,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                          width: width,
-                          child: Text("batata", textAlign: TextAlign.left,style: TextStyle(color: Colors.white,),)
-                      ),
-                      SizedBox(
-                          width: width,
-                          child: InkWell(
-                            child: Text(
-                                "Vider le panier",
-                                textAlign: TextAlign.right,
-                                style: TextStyle(color: Colors.white, fontSize: 16,decoration: TextDecoration.underline)
-                            ),
-                            onTap: (){
-                                setState(() {
-                                  reservations.clear();
-                                });
-                            },
+      body: Stack(
+        children: [
+          Container(
+            color: primaryBackgroundColor,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                              width: width,
+                              child: Text("batata", textAlign: TextAlign.left,style: TextStyle(color: Colors.white,),)
                           ),
+                          SizedBox(
+                              width: width,
+                              child: InkWell(
+                                child: Text(
+                                    "Vider le panier",
+                                    textAlign: TextAlign.right,
+                                    style: TextStyle(color: Colors.white, fontSize: 16,decoration: TextDecoration.underline)
+                                ),
+                                onTap: (){
+                                    setState(() {
+                                      reservations.clear();
+                                    });
+                                },
+                              ),
+                          ),
+                        ],
                       ),
+                    ),
+                  ),
+
+                  Expanded(
+                    flex: 7,
+                    child: reservations.length > 0 ? ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: reservations.length,
+                        itemBuilder: (context, index){
+                          return Dismissible(
+                            key: Key(reservations[index].id.toString()),
+                            child: ReservationCard(reservation: reservations[index],deleting: (){
+                              setState(() {
+                                reservations.removeAt(index);
+                              });
+                            },),
+                            onDismissed: (direction) {
+                              setState(() {
+                                reservations.removeAt(index);
+                              });
+                            },
+                          );
+                        }
+                    ) : SizedBox.expand(
+                        child: Center(
+                            child: Text("Pas encore d'endorphines par ici",
+                              style: TextStyle(color: Colors.white, fontSize: 18),)
+                        )
+                    )
+                    ,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+              bottom: 0,
+              child: Container(
+                height: height*0.11,
+                width: width,
+                decoration: BoxDecoration(
+                    color: secondaryBackgroundColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  )
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text("Total"),
+                        ],
+                      ),
+                      ElevatedButton(
+                        child: Text("Continuer"),
+                        onPressed: (){},
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(thirdBackgroundColor),
+                          fixedSize: MaterialStateProperty.all(Size(width*0.32,height*0.08)),
+                          shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)
+                          ))
+                        ),
+                      )
                     ],
                   ),
                 ),
-              ),
-
-              Expanded(
-                flex: 7,
-                child: reservations.length > 0 ? ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    itemCount: reservations.length,
-                    itemBuilder: (context, index){
-                      return Dismissible(
-                        key: Key(reservations[index].id.toString()),
-                        child: ReservationCard(reservation: reservations[index],deleting: (){
-                          setState(() {
-                            reservations.removeAt(index);
-                          });
-                        },),
-                        onDismissed: (direction) {
-                          setState(() {
-                            reservations.removeAt(index);
-                          });
-                        },
-                      );
-                    }
-                ) : SizedBox.expand(
-                    child: Center(
-                        child: Text("Pas encore d'endorphines par ici",
-                          style: TextStyle(color: Colors.white, fontSize: 18),)
-                    )
-                )
-                ,
-              ),
-            ],
+              )
           ),
-        ),
+        ],
       ),
 
     );
